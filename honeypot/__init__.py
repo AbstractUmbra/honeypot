d2809a6a21729ad3ffa576bf8855f36dbdd6146a
Alex Nørgaard <umbra@abstractumbra.dev>
2024-01-24 15:07:45 +0000

    add boilerplate

---
+++ honeypot/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import asyncio
+
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+
+from .image import generate_honeypot_image
+
+app = FastAPI(debug=True, title="Welcome!", version="0.0.1", openapi_url=None, redoc_url=None, docs_url=None)
+
+
+@app.get("/")
+async def index(request: Request) -> Response:
+    assert request.client
+
+    source_ip = request.headers.get("x-forwarded-for") or request.client.host
+    headers = request.headers
+
+    image = await asyncio.to_thread(generate_honeypot_image, source_ip, headers)
+
+    return Response(content=image.read(), media_type="image/png")
---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "honeypot:app",
+        host="0.0.0.0",
+        port=8080,
+        workers=2,
+        loop="uvloop",
+        proxy_headers=True,
+        server_header=True,
+        date_header=True,
+    )  # type: ignore # bad upstream types

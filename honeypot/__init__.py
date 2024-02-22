e09c57afd2d54cb1201e56e80cae13e6595eda54
Alex Nørgaard <umbra@abstractumbra.dev>
2024-02-22 08:29:28 +0000

    implement catchall

--- honeypot/__init__.py
+++ honeypot/__init__.py
@@ -7,19 +7,16 @@ from fastapi.responses import Response
 
 from .image import generate_honeypot_image
 
-app = FastAPI(debug=True, title="Welcome!", version="0.0.1", openapi_url=None, redoc_url=None, docs_url=None)
+app = FastAPI(debug=False, title="Welcome!", version="0.0.1", openapi_url=None, redoc_url=None, docs_url=None)
 
 
-# @app.get("/")
-@app.route("/")  # type: ignore # starlette upstream bad types
-async def index(request: Request) -> Response:
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "TRACE"])  # type: ignore # starlette upstream bad types
+async def index(request: Request, full_path: str) -> Response:
     assert request.client
 
     source_ip = request.headers.get("x-forwarded-for") or request.client.host
     headers = request.headers
 
-    image = await asyncio.to_thread(
-        generate_honeypot_image, source_ip, request.base_url.path, request.method.upper(), headers
-    )
+    image = await asyncio.to_thread(generate_honeypot_image, source_ip, request.url.path, request.method.upper(), headers)
 
     return Response(content=image.read(), media_type="image/png")
